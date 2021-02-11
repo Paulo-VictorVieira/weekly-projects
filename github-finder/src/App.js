f73a6117ef1a799f37ab478f56d6416d52990054
Paulo Victor <pvcunha05@gmail.com>
2021-02-11 18:06:52 -0300

    HTTP Requests & Updating State

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -2,13 +2,28 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Layout/Navbar';
 import User from './Components/User/User';
+import axios from 'axios';
 
 function App() {
+  const [state, setState] = React.useState({
+    users: null,
+    loading: false,
+  });
+
+  React.useEffect(() => {
+    const get = async () => {
+      setState({ loading: true });
+      const res = await axios.get('https://api.github.com/users');
+      setState({ users: res.data, loading: false });
+    };
+    get();
+  }, []);
+
   return (
     <div>
       <Navbar title="Github Finder" icon="fab fa-github" />
       <div className="container">
-        <User />
+        <User users={state.users} loading={state.loading} />
       </div>
     </div>
   );
--- github-finder/src/Components/User/User.js
+++ github-finder/src/Components/User/User.js
@@ -1,27 +1,8 @@
 import React from 'react';
 import UserItem from './UserItem';
 
-const User = () => {
-  const users = [
-    {
-      id: 1,
-      login: 'mojombo',
-      avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-      html_url: 'https://github.com/mojombo',
-    },
-    {
-      id: 2,
-      login: 'defunkt',
-      avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-      html_url: 'https://github.com/defunkt',
-    },
-    {
-      id: 3,
-      login: 'pjhyett',
-      avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-      html_url: 'https://github.com/pjhyett',
-    },
-  ];
+const User = ({ users, loading }) => {
+  if (loading) return <p>Loading...</p>;
   return (
     <div className="grid-3">
       {users && users.map((user) => <UserItem key={user.id} user={user} />)}

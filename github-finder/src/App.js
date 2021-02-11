608e2c835de2edcf6580a537d08fcccfa679770c
Paulo Victor <pvcunha05@gmail.com>
2021-02-11 17:26:14 -0300

    Setup and Axios

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import './App.css';
+import Navbar from './Components/Layout/Navbar';
+import User from './Components/User/User';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar title="Github Finder" icon="fab fa-github" />
+      <div className="container">
+        <User />
+      </div>
+    </div>
+  );
 }
 
 export default App;

e830eae33d1f804af2b42dcaa95059a0b3f8a8f9
Paulo Victor <pvcunha05@gmail.com>
2021-02-18 11:18:43 -0300

    Navbar & Router Setup

--- contact-keeper/client/src/App.js
+++ contact-keeper/client/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import { BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Layout/Navbar';
+import About from './Components/Pages/About';
+import Home from './Components/Pages/Home';
 
-function App() {
-  return <div>Hello Word</div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </>
+    </BrowserRouter>
+  );
+};
 
 export default App;

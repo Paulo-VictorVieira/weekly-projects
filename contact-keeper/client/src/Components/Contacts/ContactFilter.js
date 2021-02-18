6c80081b8a0bfe392a43e986a69f5f18b32c71da
Paulo Victor <pvcunha05@gmail.com>
2021-02-18 14:00:12 -0300

    Contacts & ContactItem Components

---
+++ contact-keeper/client/src/Components/Contacts/ContactFilter.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ContactFilter = () => {
+  return <div>Filter</div>;
+};
+
+export default ContactFilter;
--- contact-keeper/client/src/Components/Pages/Home.js
+++ contact-keeper/client/src/Components/Pages/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Contacts from '../Contacts/Contacts';
+import ContactForm from '../Contacts/ContactForm';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="grid-2 animeLeft">
+      <ContactForm />
+      <Contacts />
+    </div>
+  );
 };
 
 export default Home;

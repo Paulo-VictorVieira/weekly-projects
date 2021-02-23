2e26996b8a31c16c516629d013afa6d6d92d4f77
Paulo Victor <pvcunha05@gmail.com>
2021-02-23 19:04:50 -0300

    Update Contacts

--- contact-keeper/client/src/Context/Contact/ContactState.js
+++ contact-keeper/client/src/Context/Contact/ContactState.js
@@ -52,6 +52,27 @@ const ContactState = ({ children }) => {
     }
   };
 
+  // Update Contact
+  const updateContact = async (contact) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config,
+      );
+
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.data.msg });
+    }
+  };
+
   // Delete Contact
   const deleteContact = async (id) => {
     try {
@@ -74,11 +95,6 @@ const ContactState = ({ children }) => {
   // Clear Current Contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  // Update Contact
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   // Filter Contacts

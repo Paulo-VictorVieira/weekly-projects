c83406d6a91c2120e1de4663302d86d8215d7d8b
Paulo Victor <pvcunha05@gmail.com>
2021-03-22 14:18:52 -0300

    ToDoList & TodoItem Components

--- to-do-list/src/Components/ToDoList.js
+++ to-do-list/src/Components/ToDoList.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import TodoItem from './TodoItem';
 
-const ToDoList = () => {
-  return <div></div>;
+const ToDoList = ({ setTodos, filteredTodos }) => {
+  return (
+    <div className="todo-container">
+      {filteredTodos.length > 0 ? (
+        <ul>
+          {filteredTodos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              text={todo.text}
+              todo={todo}
+              setTodos={setTodos}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p>Add some task to do !</p>
+      )}
+    </div>
+  );
 };
 
 export default ToDoList;

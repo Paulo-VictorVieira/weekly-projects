a4a409843a98d7be7b378e516b1833ffc8767844
Paulo Victor <pvcunha05@gmail.com>
2021-03-22 20:50:22 -0300

    List Items

--- list-items/src/Components/Footer.js
+++ list-items/src/Components/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import styles from '../Styles/Footer.module.css';
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className={styles.footer}>
+      <p>Some rights reserved.</p>
+    </footer>
+  );
 };
 
 export default Footer;

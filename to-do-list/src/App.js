d5cbcfe0ec02eb15714e5a46992204688844a286
Paulo Victor <pvcunha05@gmail.com>
2021-03-22 11:05:29 -0300

    Set Up

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,8 +1,69 @@
 import React from 'react';
 import './App.css';
+import Form from './Components/Form';
+import ToDoList from './Components/ToDoList';
 
 const App = () => {
-  return <div>To Do List</div>;
+  // States
+  const [inputText, setInputText] = React.useState('');
+  const [todos, setTodos] = React.useState([]);
+  const [status, setStatus] = React.useState('all');
+  const [filteredTodos, setFilteredTodos] = React.useState([]);
+
+  // Run once when the app start
+  React.useEffect(() => {
+    const getLocalTodos = () => {
+      if (localStorage.getItem('todos') === null) {
+        localStorage.setItem('todos', JSON.stringify([]));
+      } else {
+        let todoLocal = JSON.parse(localStorage.getItem('todos'));
+        setTodos(todoLocal);
+      }
+    };
+    getLocalTodos();
+  }, []);
+
+  React.useEffect(() => {
+    const filterHandler = () => {
+      switch (status) {
+        case 'completed':
+          setFilteredTodos(todos.filter((todo) => todo.completed === true));
+          break;
+        case 'uncompleted':
+          setFilteredTodos(todos.filter((todo) => todo.completed === false));
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+      }
+    };
+
+    // Save to local
+    const saveLocalTodos = () => {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    };
+
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+
+  return (
+    <div>
+      <header>To Do List</header>
+      <Form
+        inputText={inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
+        setStatus={setStatus}
+      />
+      <ToDoList
+        todos={todos}
+        setTodos={setTodos}
+        filteredTodos={filteredTodos}
+      />
+    </div>
+  );
 };
 
 export default App;
